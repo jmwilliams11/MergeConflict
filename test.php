5355a9e63b6590eeddf93158cfcf6e1be7cb14ee
jmwilliams11 <jenna0225@gmail.com>
2021-04-07 11:18:39 -0400

    another commit after merge

--- test.php
+++ test.php
@@ -1,11 +1,8 @@
 <?php
 echo "Hello World";
 //new comment to prove file has changed
-<<<<<<< HEAD
 
 //another comment made in vs code
-?>
-=======
+
 //new comment made from github repo
 ?>
->>>>>>> 580df68287210dee3235223c490d5ae5a3d5e47c
